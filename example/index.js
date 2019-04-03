715216ae700f2313c89c634e623d80cfeb00b548
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-04-03 16:58:40 +0900

    Remove meaningless example

--- example/index.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './build/App';
-
-AppRegistry.registerComponent('example', () => App);
--- example/src/locale/languages/ja.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  title: 'Example',
-  platform: {
-    ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-    android:
-      'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-  },
-};
